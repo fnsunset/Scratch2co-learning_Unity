c79060367da29dc196accc186e4e9434aeabfa47
fnsunset <ponpon2904@gmail.com>
2016-11-18 22:40:05 +0900

    update

---
+++ Scratch2co-learning/Assets/script/scratchobject.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class scratchobject : MonoBehaviour {
+    
+    public GameObject sp_control;
+    public int obj_mem;
+    public int obj_id;
+    public float obj_x;
+    public float obj_y;
+    public float obj_z; //size
+    public float obj_r; //round
+    public float obj_d; //drection
+    public int obj_disp;
+    public Sprite obj_sprite;
+    float obj_rx = 0f;
+    float obj_ry = 0f;
+    float obj_sizex;
+    float obj_sizey;
+    float[] screen = new float[4];
+    float[,] obj_edge = new float[4,2];
+    SpriteRenderer thisrender;
+    socketio sp_con;
+    bool ch_sprite= false;
+    // Use this for initialization
+    void Start () {
+        thisrender = gameObject.GetComponent<SpriteRenderer>();
+        sp_control = GameObject.Find("connect_server");
+        sp_con = sp_control.GetComponent<socketio>();
+        obj_sizex = thisrender.bounds.size.x;
+        obj_sizey = thisrender.bounds.size.y;
+        switch (obj_mem)
+        {
+            case 0:
+                screen[0] = sp_con.scsize_x * -1;
+                screen[1] = sp_con.scsize_y;
+                screen[2] = 0f;
+                screen[3] = 0f;
+                break;
+            case 1:
+                screen[0] = 0f;
+                screen[1] = sp_con.scsize_y;
+                screen[2] = 0f;
+                screen[3] = sp_con.scsize_x;
+                break;
+            case 2:
+                screen[0] = sp_con.scsize_x * -1;
+                screen[1] = 0f;
+                screen[2] = sp_con.scsize_y * -1;
+                screen[3] = 0f;
+                break;
+            case 3:
+                screen[0] = 0f;
+                screen[1] = 0f;
+                screen[2] = sp_con.scsize_y * -1;
+                screen[3] = sp_con.scsize_x * -1;
+                break;
+        }
+        screen[0] = screen[0] * sp_con.disp_x / sp_con.scsize_x;    //左の限界座標 - Scratch系
+        screen[1] = screen[1] * sp_con.disp_y / sp_con.scsize_y;    //上の限界座標 - Scratch系
+        screen[2] = screen[2] * sp_con.disp_y / sp_con.scsize_y;    //下の限界座標 - Scratch系
+        screen[3] = screen[3] * sp_con.disp_x / sp_con.scsize_x;    //右の限界座標 - Scratch系
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(ch_sprite == false && obj_sprite)
+        {
+            thisrender.sprite = obj_sprite;
+            this.transform.localScale = new Vector3(obj_z, obj_z, 1);
+            ch_sprite = true;
+        }
+        obj_x = sp_con.obj_x[obj_mem, obj_id];
+        obj_y = sp_con.obj_y[obj_mem, obj_id];
+        obj_z = sp_con.obj_z[obj_mem, obj_id];
+        obj_r = sp_con.obj_r[obj_mem, obj_id];
+        obj_d = sp_con.obj_d[obj_mem, obj_id];
+        obj_disp = sp_con.obj_disp[obj_mem, obj_id];
+        Vector3 obj_pos = transform.position;
+        obj_pos.x = obj_x * sp_con.scsize_x / sp_con.disp_x;    //Unity系座標に変換
+        obj_pos.y = obj_y * sp_con.scsize_y / sp_con.disp_y;    //Unity系座標に変換
+        transform.position = obj_pos;
+        transform.localScale = new Vector3(obj_z, obj_z, 1);
+        transform.rotation = Quaternion.Euler(obj_rx, obj_ry, obj_r);
+        var color = thisrender.color;
+        if (obj_disp >0)
+        {          
+            color.a = 1f;
+        }
+        else
+        {
+            color.a = 0f;
+        }
+        thisrender.color = color;
+        obj_sizex = thisrender.bounds.size.x * sp_con.disp_x / sp_con.scsize_x;
+        obj_sizey = thisrender.bounds.size.y * sp_con.disp_y / sp_con.scsize_y;
+        obj_edge[0, 0] = obj_x - obj_sizex * Mathf.Cos(obj_r * Mathf.Deg2Rad);          //オブジェクトの左上x座標 - scratch系
+        obj_edge[0, 1] = obj_y + obj_sizey * Mathf.Sin(obj_r * Mathf.Deg2Rad);          //オブジェクトの左上y座標 - scratch系
+        obj_edge[1, 0] = obj_x + obj_sizex * Mathf.Cos((90f - obj_r) * Mathf.Deg2Rad);  //オブジェクトの右上x座標 - scratch系
+        obj_edge[1, 1] = obj_y + obj_sizey * Mathf.Sin((90f - obj_r) * Mathf.Deg2Rad);  //オブジェクトの右上y座標 - scratch系
+        obj_edge[2, 0] = obj_x - obj_sizex * Mathf.Cos((90f - obj_r) * Mathf.Deg2Rad);  //オブジェクトの左下x座標 - scratch系
+        obj_edge[2, 1] = obj_y - obj_sizey * Mathf.Sin((90f - obj_r) * Mathf.Deg2Rad);  //オブジェクトの左下y座標 - scratch系
+        obj_edge[3, 0] = obj_x + obj_sizex * Mathf.Cos(obj_r * Mathf.Deg2Rad);          //オブジェクトの右下x座標 - scratch系
+        obj_edge[3, 1] = obj_y - obj_sizey * Mathf.Sin(obj_r * Mathf.Deg2Rad);          //オブジェクトの右下y座標 - scratch系
+    }
+}
--- Scratch2co-learning/Assets/script/socketio.cs
+++ Scratch2co-learning/Assets/script/socketio.cs
@@ -1,16 +1,82 @@
 ﻿using UnityEngine;
 using SocketIO;
-using System.Collections;
+using System;
 
 public class socketio : MonoBehaviour {
     SocketIOComponent socket;
     string id = "";
+    bool flame = true;
+    public int maxmember = 4;
+    public int Group;
+    public int disp_x;
+    public int disp_y;
+    public float scsize_x;
+    public float scsize_y;
+    public GameObject camerapos;
+    public GameObject addobject;
+    public GameObject obj_parent;
+    public Sprite[] Sprites;
+    public float[] Sprite_Size;
+    public float[,] obj_x = new float[10,50];
+    public float[,] obj_y = new float[10,50];
+    public float[,] obj_z = new float[10,50]; //size
+    public float[,] obj_r = new float[10,50]; //round
+    public float[,] obj_d = new float[10,50]; //drection
+    public int[,] obj_disp = new int[10,50];
+    GameObject clone;
     // Use this for initialization
     void Start () {
         GameObject go = GameObject.Find("SocketIO");
         socket = go.GetComponent<SocketIOComponent>();
         socket.On("server/hello", hello);
+        socket.On("unity/move", socket_move);
+        socket.On("unity/rotate", socket_rotate);
+        socket.On("unity/ang", socket_angle);
+        socket.On("unity/movex", socket_movex);
+        socket.On("unity/movey", socket_movey);
+        socket.On("unity/warp", socket_warp);
         Debug.Log("Send first message");
+        get_cam_pos campos = camerapos.GetComponent<get_cam_pos>();
+        scsize_x = campos.ScreenBottomRight.x;
+        scsize_y = campos.ScreenTopLeft.y;
+        for (int cnta = 0; cnta < Sprites.Length; cnta++)
+        {
+            for(int cntb = 0; cntb < maxmember; cntb++)
+            {
+                clone = (GameObject)Instantiate(addobject);
+                clone.transform.parent = obj_parent.transform;
+                scratchobject sc_obj = clone.GetComponent<scratchobject>();
+                sc_obj.obj_id = cnta;
+                sc_obj.obj_mem = cntb;
+                if(maxmember == 4)
+                {
+                    switch (cntb)
+                    {
+                        case 0:
+                            obj_x[cntb,cnta] = disp_x /-2f;
+                            obj_y[cntb, cnta] = disp_y /2f;
+                            break;
+                        case 1:
+                            obj_x[cntb, cnta] = disp_x / 2f;
+                            obj_y[cntb, cnta] = disp_y / 2f;
+                            break;
+                        case 2:
+                            obj_x[cntb, cnta] = disp_x / -2f;
+                            obj_y[cntb, cnta] = disp_y / -2f;
+                            break;
+                        case 3:
+                            obj_x[cntb, cnta] = disp_x / 2f;
+                            obj_y[cntb, cnta] = disp_y / -2f;
+                            break;
+                    }
+                }
+                obj_z[cntb, cnta] = Sprite_Size[cnta];
+                obj_r[cntb, cnta] = 0f;
+                obj_d[cntb, cnta] = 0f;
+                obj_disp[cntb, cnta] = -1;
+                sc_obj.obj_sprite = Sprites[cnta];
+            }
+        }
     }
 	
 	// Update is called once per frame
@@ -23,9 +89,115 @@ public class socketio : MonoBehaviour {
         id = obj.GetField("id").str;
         Debug.Log("response from: " + from + "your ID:"+id);
 
-        //ちょっと変えるよここは最終的に
         JSONObject jsonObject = new JSONObject(JSONObject.Type.OBJECT);
         jsonObject.AddField("id", id);
-        socket.Emit("scratch/hello", jsonObject);
+        jsonObject.AddField("group", Group);
+        socket.Emit("unity/hello", jsonObject);
+    }
+    public void socket_move(SocketIOEvent e)
+    {
+        Debug.Log("moveを受信しました ");
+        JSONObject obj = e.data;
+        float socket_group = obj.GetField("Group").n;
+        if(socket_group == (float)Group)
+        {
+            float socket_move = obj.GetField("Move").n;
+            int socket_num = (int)obj.GetField("Number").n;
+            int socket_obj = (int)obj.GetField("Obj").n;
+            obj_x[socket_num, socket_obj] += socket_move * Mathf.Cos(obj_r[socket_num, socket_obj]*Mathf.Deg2Rad);
+            obj_y[socket_num, socket_obj] += socket_move * Mathf.Sin(obj_r[socket_num, socket_obj] * Mathf.Deg2Rad);
+            if(obj_disp[socket_num, socket_obj] == -1)
+            {
+                obj_disp[socket_num, socket_obj] = 1;
+            }
+            Debug.Log("moveを受信した結果、"+socket_num+"の"+socket_obj+"を"+socket_move+"歩動かしました");
+        }
+    }
+    public void socket_rotate(SocketIOEvent e)
+    {
+        Debug.Log("rotateを受信しました ");
+        JSONObject obj = e.data;
+        float socket_group = obj.GetField("Group").n;
+        if (socket_group == (float)Group)
+        {
+            float socket_rotate = obj.GetField("Rotate").n;
+            int socket_num = (int)obj.GetField("Number").n;
+            int socket_obj = (int)obj.GetField("Obj").n;
+            obj_r[socket_num, socket_obj] += socket_rotate;
+            if (obj_disp[socket_num, socket_obj] == -1)
+            {
+                obj_disp[socket_num, socket_obj] = 1;
+            }
+        }
+    }
+    public void socket_angle(SocketIOEvent e)
+    {
+        Debug.Log("angleを受信しました ");
+        JSONObject obj = e.data;
+        float socket_group = obj.GetField("Group").n;
+        if (socket_group == (float)Group)
+        {
+            float socket_angle = obj.GetField("Angle").n;
+            int socket_num = (int)obj.GetField("Number").n;
+            int socket_obj = (int)obj.GetField("Obj").n;
+            obj_r[socket_num, socket_obj] = socket_angle;
+            if (obj_disp[socket_num, socket_obj] == -1)
+            {
+                obj_disp[socket_num, socket_obj] = 1;
+            }
+        }
+    }
+    public void socket_movex(SocketIOEvent e)
+    {
+        Debug.Log("movexを受信しました ");
+        JSONObject obj = e.data;
+        float socket_group = obj.GetField("Group").n;
+        if (socket_group == (float)Group)
+        {
+            float socket_movex = obj.GetField("Movex").n;
+            int socket_num = (int)obj.GetField("Number").n;
+            int socket_obj = (int)obj.GetField("Obj").n;
+            obj_x[socket_num, socket_obj] += socket_movex;
+            if (obj_disp[socket_num, socket_obj] == -1)
+            {
+                obj_disp[socket_num, socket_obj] = 1;
+            }
+        }
+    }
+    public void socket_movey(SocketIOEvent e)
+    {
+        Debug.Log("moveyを受信しました ");
+        JSONObject obj = e.data;
+        float socket_group = obj.GetField("Group").n;
+        if (socket_group == (float)Group)
+        {
+            float socket_movey = obj.GetField("Movey").n;
+            int socket_num = (int)obj.GetField("Number").n;
+            int socket_obj = (int)obj.GetField("Obj").n;
+            obj_y[socket_num, socket_obj] += socket_movey;
+            if (obj_disp[socket_num, socket_obj] == -1)
+            {
+                obj_disp[socket_num, socket_obj] = 1;
+            }
+        }
+    }
+    public void socket_warp(SocketIOEvent e)
+    {
+        Debug.Log("warpを受信しました ");
+        JSONObject obj = e.data;
+        float socket_group = obj.GetField("Group").n;
+        if (socket_group == (float)Group)
+        {
+            float socket_warpx = obj.GetField("Warpx").n;
+            float socket_warpy = obj.GetField("Warpy").n;
+            int socket_num = (int)obj.GetField("Number").n;
+            int socket_obj = (int)obj.GetField("Obj").n;
+            obj_x[socket_num, socket_obj] = socket_warpx;
+            obj_y[socket_num, socket_obj] = socket_warpy;
+            if (obj_disp[socket_num, socket_obj] == -1)
+            {
+                obj_disp[socket_num, socket_obj] = 1;
+            }
+        }
     }
 }
